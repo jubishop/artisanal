a65770d421374eaa55ab5c85bf2c956af0ebf0f5
Justin Bishop <jubishop@gmail.com>
2020-09-02 18:31:02 -0700

    fix up some links

--- config.rb
+++ config.rb
@@ -5,14 +5,18 @@ page '/*.json', layout: false
 
 activate :blog do |blog|
   blog.calendar_template = 'calendar.html'
+  blog.day_link = "date/{year}/{month}/{day}.html"
   blog.default_extension = '.md.erb'
+  blog.month_link = "date/{year}/{month}.html"
   blog.new_article_template = File.expand_path(
     'source/templates/article.tt',
     File.dirname(__FILE__))
   blog.sources = 'articles/{title}.html'
-  blog.permalink = '{title}.html'
+  blog.permalink = 'article/{title}.html'
+  blog.taglink = "tag/{tag}.html"
   blog.tag_template = 'tag.html'
   blog.paginate = true
+  blog.year_link = "date/{year}.html"
 end
 
 config[:css_dir] = 'assets/css'
