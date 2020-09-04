c59dd428ef941de1cbac399579bc4d116c00d6d2
Justin Bishop <jubishop@gmail.com>
2020-09-03 23:19:29 -0700

    sort out the js better

--- source/assets/js/site.js
+++ source/assets/js/site.js
@@ -2,28 +2,27 @@ class SideBar {
   static domLoaded() {
     const showButton = document.getElementById('show-sidebar-button');
     const hideButton = document.getElementById('hide-sidebar-button');
-
     const sidebarLinks = document.getElementById('sidebar-links');
     const sidebarHeader = document.getElementById('sidebar-header');
     const cornerLetter = document.getElementById('corner-artisanal-letter');
     const headlineLetter = document.getElementById('headline-artisanal-letter');
 
     showButton.addEventListener('click', () => {
-      sidebarLinks.style.display = 'block';
-      sidebarHeader.style.display = 'none';
       showButton.style.display = 'none';
       hideButton.style.display = 'block';
+      sidebarLinks.style.display = 'block';
+      sidebarHeader.style.display = 'none';
       cornerLetter.style.display = 'block';
       headlineLetter.style.display = 'none';
     });
 
     hideButton.addEventListener('click', () => {
-      sidebarLinks.style.display = 'none';
-      sidebarHeader.style.display = 'block';
       showButton.style.display = 'block';
       hideButton.style.display = 'none';
+      sidebarLinks.style.display = 'none';
+      sidebarHeader.style.display = 'block';
       cornerLetter.style.display = 'none';
-      headlineLetter.style.display = 'inline';
+      headlineLetter.style.display = 'inline-block';
     });
   }
 }
