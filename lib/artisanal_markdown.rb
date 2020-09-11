646b518c0b2ff984ba30f815a9c21f66438e2f69
Justin Bishop <jubishop@gmail.com>
2020-09-10 22:14:22 -0700

    custom inline code formatting done

--- lib/artisanal_markdown.rb
+++ lib/artisanal_markdown.rb
@@ -1,9 +1,18 @@
 require 'middleman-core/renderers/redcarpet'
+require 'rouge'
 
 class ArtisanalMarkdown < Middleman::Renderers::MiddlemanRedcarpetHTML
   def preprocess(full_document)
+    # Iconify
     full_document.gsub!(/!iconify\[(.+?)\]/,
                         '<iconify-icon data-icon="\1"></iconify-icon>')
+
+    # Fancy inline code
+    formatter = Rouge::Formatters::HTML.new
+    full_document.gsub!(/(?<language>[a-z]+)\|`(?<code>.+?)`\|/) {
+      inline_code($~[:code], $~[:language])
+    }
+
     return full_document
   end
 
@@ -14,6 +23,15 @@ class ArtisanalMarkdown < Middleman::Renderers::MiddlemanRedcarpetHTML
   end
 
   def codespan(code)
-    "<span class=\"codespan\"><code>#{code}</code></span>"
+    return inline_code(code, 'ruby')
+  end
+
+  private
+
+  def inline_code(code, language)
+    formatter = Rouge::Formatters::HTML.new
+    lexer = Rouge::Lexer.find_fancy(language, code)
+    formatted = formatter.format(lexer.lex(code))
+    return "<span class=\"highlight\"><code>#{formatted}</code></span>"
   end
 end
